9e6650f0b1e8d58382df61dd4c82ecb9ed916883
SiriusLL <sirius.love.light@gmail.com>
2021-03-12 19:21:15 +0000

    added .this and refactored functions into object method

--- index.js
+++ index.js
@@ -3,21 +3,17 @@ function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
   };
   return task;
-}
-
-
-// prints out the provided task's details
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-}
-
-
-// marks the provided task as completed
-function completeTask(task) {
-  task.complete = true;
 }
 
 
@@ -27,8 +23,8 @@ const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter b
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-logTaskState(task1); // Clean Cat Litter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
 
-console.log(tasks);
+
